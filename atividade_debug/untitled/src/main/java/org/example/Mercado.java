6d3b7e1c95beae962f765c732882c1935e56e2f9
David Fernandes <dfgamerwolf12@gmail.com>
2026-04-09 11:26:30 -0300

    Adicionando o arquivo main.java modificada e o arquvio ALTERACOES.md

--- atividade_debug/untitled/src/main/java/org/example/Mercado.java
+++ atividade_debug/untitled/src/main/java/org/example/Mercado.java
@@ -1,35 +1,8 @@
 package org.example;
 import java.util.Scanner;
 public class Mercado {
-        public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
-            System.out.print("Digite o nome do produto: ");
-            String nomeProduto = sc.nextLine();
-            System.out.print("Digite o preço do produto: ");
-            double preco = sc.nextDouble();
-            System.out.print("Digite a quantidade: ");
-            int quantidade = sc.nextInt();
-            System.out.print("Digite o valor mínimo para desconto: ");
-            double descontoMinimo = sc.nextDouble();
-
-            double total = calcularTotal(preco, quantidade);
-            if (total < descontoMinimo) {
-                double desconto = total * 0.10;
-                total = total + desconto;
-            }
-            total = total + 15;
-            System.out.println("\nProduto: " + nomeProduto);
-            System.out.println("Total da compra: R$ " + total);
-            quantidade = atualizarQuantidade(quantidade);
-            System.out.println("Quantidade final: " + quantidade);
-            sc.close();
-        }
-        public static double calcularTotal(double preco, int quantidade) {
-            double total = preco * quantidade + 5;
-            return total;
-        }
-        public static int atualizarQuantidade(int qtd) {
-
-            return qtd - 2;
-        }
+    public static double calcularTotal(double preco, int quantidade) {
+        double total = preco * quantidade;
+        return total;
     }
+}
